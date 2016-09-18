447bfa76d54b5a15eecb351cfb5174a57093ff5a
Jennifer Owens <jmo.jennifer@gmail.com>
2016-09-17 17:08:11 -0700

    Added input verification spec tests for initialize method

--- specs/door_spec.rb
+++ specs/door_spec.rb
@@ -2,24 +2,57 @@ require_relative 'spec_helper'
 
 describe "door" do
   before (:each) do
-    @door = Door.new
+    @door_open_unlocked = Door.new("open", "unlocked")
+    # @door_open_locked = Door.new("open", "locked"
+    # @door_closed_unlocked = Door.new("closed", "unlocked")
+    # @door_closed_locked = Door.new("closed", "locked")
   end
 
   describe "#initialize" do
     it "can create a new instance of Door" do
-      @door.must_be_instance_of(Door)
+      @door_open_unlocked.must_be_instance_of(Door)
     end
+
+    it "should handle strings consistently regardless of capitalization" do
+      @capitalization_door = Door.new("OPEN", "UNLOCKED")
+      @capitalization_door.position.must_equal("open")
+      @capitalization_door.position.must_equal("unlocked")
+    end
+
+    it "should be initalized with one of two values for position: open and closed" do
+      proc { @attribute_check_door = Door.new("Ajar", "unlocked") }.must_raise NoMethodError
+    end
+
+    it "should be initalized with one of two values for lock_status: locked and unlocked" do
+      proc { @attribute_check_door = Door.new("open", '¯\_(ツ)_/¯') }.must_raise NoMethodError
+    end
+
   end
 
-  describe "set_inscription" do
+  describe "#set_inscription" do
     it "should set the inscription variable to a non-nil value if it is currently nil" do
-      @door.set_inscription("Your Message Here").must_equal("Your Message Here")
+      @door_open_unlocked.set_inscription("Your Message Here").must_equal("Your Message Here")
     end
 
     it "should not allow the inscription to be changed once set" do
-      @door.set_inscription("Your Message Here")
-      @door.set_inscription("Can I do the Thing?").wont_equal("Can I do the Thing?")
+      @door_open_unlocked.set_inscription("Your Message Here")
+      @door_open_unlocked.set_inscription("Can I do the Thing?").wont_equal("Can I do the Thing?")
     end
   end
+
+  # describe "change_door_position" do
+  #   it "should have one of two values for position: open and closed" do
+  #
+  #   end
+  #
+  #   it "should have one of two values for lock_status: locked and unlocked" do
+  #
+  #   end
+  #
+  #   it "should toggle the door position between the closed and open states" do
+  #     @door_open_unlocked.change_door_position
+  #   end
+  #
+  # end
 
 end
