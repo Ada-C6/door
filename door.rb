cd46325c911a99d96c1ff292dbeb7975b634a5d1
Dianne Laguerta <diannelaguerta@gmail.com>
2016-09-18 10:27:06 -0700

    wrote beginning initialize method, method to check lockedness, and method to unlock door

--- door.rb
+++ door.rb
@@ -1,5 +1,45 @@
 class Door
-  def initialize
-    
+  attr_reader :inscription
+  attr_accessor :lock, :access
+
+  def initialize(inscription)
+    @inscription = inscription
+    @lock = true
   end
-end
+
+  def locked?
+    if @lock == true
+      # puts "This door is locked to you!"
+      return true
+    else
+      # puts "Door is unlocked! Cool beans!"
+      return false
+    end
+  end #end locked? method
+
+  def unlock
+    #it should raise an error here if @lock is already false. can remove the else then.
+    if @lock == true
+      @lock == false
+      return "door is now unlocked. Yay."
+    else
+      return "Door is already unlocked."
+    end
+  end
+
+  # def open?
+  #   it should check if the door is open or closed
+
+  # def gain_access
+    #this method will open the door if:
+    #the door must be unlocked already, and not open
+    #if it's locked and closed, user cannot gain access
+    #
+
+
+
+end #end class
+
+#we should give the door an inscription at its inception, not allow a player to change the inscription, which can be
+
+#
--- specs/door_spec.rb
+++ specs/door_spec.rb
@@ -3,9 +3,42 @@ require_relative '../door'
 
 class Door
   describe "#initialize" do
-    let(:door) {Door.new}
+  let(:door) {Door.new("you shall not pass!")}
     it "should create an instance of a door" do
       door.must_be_instance_of(Door)
     end
+
+    it "should be given an inscrption at inception with an inscrption attribute" do
+      door.must_respond_to(:inscription)
+    end
+
+    # it "should not allow for its inscription to be written over" do
+    #
+    # end
   end
+
+  describe "#locked?" do
+    let(:door) {Door.new("this is a door.")}
+    it "should be locked at default" do
+      door.locked?.must_equal(true)
+    end
+  end
+
+  describe "#unlock" do
+    let(:door) {Door.new("this is a door.")}
+    it "should raise an error if the door is already unlocked" do
+      proc {door.lock = false}.must_raise Exception
+    end
+    it "should be unlock when door is locked" do
+      door.unlock.must_equal("door is now unlocked. Yay.")
+    end
+
+    it "should tell user it is already unlocked when unlocked" do
+      door.lock = false
+      door.unlock.must_equal("Door is already unlocked.")
+    end
+
+
+  end
+
 end #end class
