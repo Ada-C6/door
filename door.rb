c399fa79e0b24d1b6b58f5fcd69646954a2a13da
Esther Ng <esther.hc.ng@gmail.com>
2016-09-17 18:57:25 -0700

    Completed initialize spec with attributes

---
+++ door.rb
@@ -0,0 +1,12 @@
+class Door
+  attr_reader :closed, :locked, :inscription
+
+  def initialize
+    @closed = true
+    @locked = false
+    @inscription = ""
+  end
+
+  # def closed?
+
+end
--- specs/door_spec.rb
+++ specs/door_spec.rb
@@ -1,2 +1,33 @@
 require_relative 'spec_helper'
 # require_relative '../door.rb'
+
+describe Door do
+
+  describe "#initialize" do
+    let(:door) { Door.new }
+
+    it "can create a new instance of Door" do
+      door.must_be_instance_of(Door)
+    end
+
+    it "should allow user to check if door is closed" do
+      door.must_respond_to(:closed)
+    end
+
+    it "should allow user to check if door is locked" do
+      door.locked.class.must_equal(FalseClass)
+    end
+
+    it "should initialize with an empty inscription" do
+      door.inscription.must_be(:==, "")
+    end
+
+  end
+
+  # describe "#closed?" do
+  #   it "should check if "
+  #
+  #
+  # end
+
+end
