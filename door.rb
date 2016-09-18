45fe2e760c73aec923997fac9fbf27e7a02770d5
Jennifer Owens <jmo.jennifer@gmail.com>
2016-09-17 17:45:00 -0700

    Initialize method now checks for valid inputs; 100% spec coverage

--- door.rb
+++ door.rb
@@ -1,20 +1,36 @@
 class Door
   attr_reader :inscription, :position, :lock_status
 
-  def initialize
+  def initialize(position, lock_status)
     @inscription = nil
-    @position = "closed"
-    @lock_status = "locked"
+
+    if position.downcase == "open" || position.downcase == "closed"
+      @position = position.downcase
+    else
+      raise Exception("Invalid attribute value")
+    end
+
+    if lock_status.downcase == "unlocked" || lock_status.downcase == "locked"
+      @lock_status = lock_status.downcase
+    else
+      raise Exception("Invalid attribute value")
+    end
+  end
+
+
+
+  def inscription?
+    return @inscription != nil
   end
 
   def set_inscription(inscription)
-    if @inscription == nil
-      @inscription = inscription
+    if inscription?
+      puts "The inscription is set, and cannot be changed."
       return @inscription
     else
-      puts "The inscription is set, and cannot be changed."
+      @inscription = inscription
       return @inscription
     end
   end
-  
+
 end
