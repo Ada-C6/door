603c8f60929e1e01e4cf6d4d4b1184ff1e87863c
Nicole Url <nicole.url.4@gmail.com>
2016-09-18 19:57:39 -0700

    Added locked? and opened? methods, and an open method

--- door.rb
+++ door.rb
@@ -1,12 +1,36 @@
 class Door
-  attr_reader :inscription, :locked, :open
+  attr_reader :inscription, :locked, :opened
 
   def initialize (inscription)
     @inscription = inscription
     @locked = false
-    @open = true
+    @opened = true
+    @actions = ["open","close","lock","unlock","o","c","l","u"]
   end
 
+  def locked?
+    @locked == true
+  end
+
+  def opened?
+    @opened == true
+  end
+
+  # def input(action.strip.downcase)
+  #   raise ArgumentError.new("Please choose an action this door can accept.") unless @actions.include?(action)
+  #
+  #   return action
+  # end
+
+  def open
+    raise ArgumentError.new("The door is already open") if opened? == true
+
+    if locked? == true
+      return false
+    else
+      return true
+    end
+  end
 
 
 end
--- specs/door_spec.rb
+++ specs/door_spec.rb
@@ -8,13 +8,29 @@ describe Door do
     end
 
     it "should create an unlocked door" do
-      Door.new("bathroom").locked.must_equal false
+      Door.new("bathroom").locked?.must_equal false
     end
 
     it "should create an open door" do
-      Door.new("bathroom").open.must_equal true
+      Door.new("bathroom").opened.must_equal true
     end
   end
 
+  # describe "#input" do
+  #   it "should raise an Argument Error if the user is trying to perform an action it can't handle" do
+  #     proc { door.input("fart") }.must_raise(ArgumentError)
+  #   end
+  #
+  #   it "should return a valid action" do
+  #     door = Door.new("bathroom")
+  #     door.input("OPEN").must_equal "open"
+  #   end
+  # end
 
+  describe "#open" do
+    it "should raise an ArgumentError if the door is already opened" do
+      door = Door.new("bathroom")
+      proc { door.open }.must_raise(ArgumentError)
+    end
+  end
 end
