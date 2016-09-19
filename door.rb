3e426022765cbac84f6cbeb2a98e7ea7a59a3f63
Nicole Url <nicole.url.4@gmail.com>
2016-09-18 19:59:28 -0700

    Made open a self method

--- door.rb
+++ door.rb
@@ -1,7 +1,7 @@
 class Door
   attr_reader :inscription, :locked, :opened
 
-  def initialize (inscription)
+  def initialize(inscription)
     @inscription = inscription
     @locked = false
     @opened = true
@@ -16,13 +16,7 @@ class Door
     @opened == true
   end
 
-  # def input(action.strip.downcase)
-  #   raise ArgumentError.new("Please choose an action this door can accept.") unless @actions.include?(action)
-  #
-  #   return action
-  # end
-
-  def open
+  def self.open
     raise ArgumentError.new("The door is already open") if opened? == true
 
     if locked? == true
--- specs/door_spec.rb
+++ specs/door_spec.rb
@@ -16,18 +16,7 @@ describe Door do
     end
   end
 
-  # describe "#input" do
-  #   it "should raise an Argument Error if the user is trying to perform an action it can't handle" do
-  #     proc { door.input("fart") }.must_raise(ArgumentError)
-  #   end
-  #
-  #   it "should return a valid action" do
-  #     door = Door.new("bathroom")
-  #     door.input("OPEN").must_equal "open"
-  #   end
-  # end
-
-  describe "#open" do
+  describe "#self.open" do
     it "should raise an ArgumentError if the door is already opened" do
       door = Door.new("bathroom")
       proc { door.open }.must_raise(ArgumentError)
