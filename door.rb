bc9c30de3c67a2cc7c7097671f19e52b392e1881
heatherherrington <redherring@gmail.com>
2016-09-19 12:48:07 -0400

    Open, close, lock, unlock method and specs written

--- door.rb
+++ door.rb
@@ -1,5 +1,69 @@
-class door
+class Door
 
+  attr_reader
+  attr_accessor
+
+  def initialize
+    @locked = false
+    @open = false
+  end
+
+  def open_if_allowed
+    if locked?
+      raise ArgumentError.new("You cannot open the door when it is locked.")
+    elsif closed? == false
+      raise ArgumentError.new("You cannot open an already open door.")
+    else
+      @open = true
+    end
+  end
+
+  def close_if_allowed
+    if locked?
+      raise ArgumentError.new("The deadbolt is locked, so the door cannot close.")
+    elsif closed? == false
+      raise ArgumentError.new("You cannot close an already closed door.")
+    else
+      @open = trie
+    end
+  end
+
+  def unlock_door
+    if locked? == false
+      raise ArgumentError.new("The door is already unlocked.")
+    else
+      @locked = false
+    end
+  end
+
+  def lock_door
+    if locked?
+      raise ArgumentError.new("The door is already locked.")
+    else
+      @locked = true
+    end
+  end
+
+  def locked?
+    @locked = true
+  end
+
+  def closed?
+    @open = false
+  end
 
 
 end
+
+
+# A Door object can
+#
+# display an inscription
+# be either open or closed, and
+# be either locked or unlocked
+#
+# Once the writing (inscription) on a Door is set, it cannot be changed
+# You should be able to look at the writing on the Door if there is any.
+# Appropriate error messages should be displayed and no changes to the Door should be made, if any conditions of the functions are violated.
+#
+# 90% test coverage (using simplecov).
--- specs/door_spec.rb
+++ specs/door_spec.rb
@@ -2,7 +2,63 @@ require_relative 'spec_helper'
 require_relative '../door'
 
 describe Door do
+  describe "#initialize" do
+    let(:door) { Door.new }
+
+    it "can create a new instance of Game" do
+      door.must_be_instance_of(Door)
+    end
+  end
+
+  describe "#open_if_allowed" do
+    let(:door) { Door.new }
+
+    it "should raise an Argument Error if locked? is true" do
+      proc { door.open_if_allowed }.must_raise(ArgumentError)
+    end
+
+    it "should raise an Argument Error if closed? is true" do
+      proc { door.open_if_allowed }.must_raise(ArgumentError)
+    end
+  end
+
+  describe "#close_if_allowed" do
+    let(:door) { Door.new }
+
+    it "should raise an Argument Error if locked? is true" do
+      proc { door.close_if_allowed }.must_raise(ArgumentError)
+    end
+
+    it "should raise an Argument Error if closed? is true" do
+      proc { door.close_if_allowed }.must_raise(ArgumentError)
+    end
+  end
+
+  describe "#unlock_door" do
+    let (:door) { Door.new }
+
+    it "should raise an Argument Error if locked? is false" do
+      proc { door.open_if_allowed }.must_raise(ArgumentError)
+    end
+
+    it "should allow the user to unlock the door" do
+      door.unlock_door.must_equal(false)
+    end
+
+    describe "#lock_door" do
+      let (:door) { Door.new }
+
+      it "should raise an Argument Error if locked? is true" do
+        proc { door.lock_door }.must_raise(ArgumentError)
+      end
+
+      it "should allow the user to lock the door" do
+        if @locked == false
+          door.lock_door.must_equal(true)
+        end
+      end
+    end
 
 
-  
+  end
 end
