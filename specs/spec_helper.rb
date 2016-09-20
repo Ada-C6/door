3fc580682c0ffdca7dbb26f5870aaa6121ca3018
Shari Meggs <smeggs1@gmail.com>
2016-09-19 18:07:16 -0700

    Spec helper has the mini test info as well as the link to doors

---
+++ specs/spec_helper.rb
@@ -0,0 +1,13 @@
+require 'simplecov'
+
+SimpleCov.start
+
+require 'minitest'
+require 'minitest/spec'
+require "minitest/autorun"
+require "minitest/reporters"
+require 'minitest/pride'
+#These are all the things about mini test that we are using
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative '../doors'
