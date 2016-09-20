656989b29c4fd822a573bb451daa67c3f7e6e713
Dianne Laguerta <diannelaguerta@gmail.com>
2016-09-20 10:03:00 -0700

    added working methods and tests for locking and unlocking door

--- door.rb
+++ door.rb
@@ -1,45 +1,67 @@
 class Door
   attr_reader :inscription
-  attr_accessor :lock, :access
+  attr_accessor :locked_door, :closed_door
 
   def initialize(inscription)
     @inscription = inscription
-    @lock = true
+    @locked_door = locked_door
+    @locked_door = true #means door is locked
+    @closed_door = true
   end
 
   def locked?
-    if @lock == true
-      # puts "This door is locked to you!"
-      return true
+    # puts "#{@locked_door}"
+    if @locked_door == true
+       puts "This door is locked to you!"
+      return @locked_door
     else
-      # puts "Door is unlocked! Cool beans!"
+       puts "Door is unlocked! Cool beans!"
       return false
     end
   end #end locked? method
 
   def unlock
-    #it should raise an error here if @lock is already false. can remove the else then.
-    if @lock == true
-      @lock == false
+    if @locked_door == false
+      raise ArgumentError.new("Door is unlocked. You cannot unlock door if door is already unlocked.")
+    end
+
+    if @locked_door == true
+      @locked_door = false
       return "door is now unlocked. Yay."
-    else
-      return "Door is already unlocked."
+    # else
+    #   return "Door is already unlocked."
     end
   end
 
-  # def open?
-  #   it should check if the door is open or closed
+  def lock #method to lock the door
+    if @locked_door == true
+  raise ArgumentError.new("You cannot lock an already locked door.")
+    end
 
-  # def gain_access
-    #this method will open the door if:
-    #the door must be unlocked already, and not open
-    #if it's locked and closed, user cannot gain access
-    #
+    if @locked_door == false
+       @locked_door = true
+      return "Door is now locked."
+    end
 
 
+  end #end for lock method
+
+# def open?
+#   it should check if the door is open or closed
+
+# def gain_access
+#this method will open the door if:
+#the door must be unlocked already, and not open
+#if it's locked and closed, user cannot gain access
+#
 
 end #end class
 
 #we should give the door an inscription at its inception, not allow a player to change the inscription, which can be
 
-#
+
+#it should raise an error here if @locked_door is already false. can remove the else then.
+
+#   unless @lock == true
+#    raise ArgumentError.new("Cannot unlock door if door is already unlocked.")
+#  end
--- specs/door_spec.rb
+++ specs/door_spec.rb
@@ -8,37 +8,62 @@ class Door
       door.must_be_instance_of(Door)
     end
 
-    it "should be given an inscrption at inception with an inscrption attribute" do
+    it "should be given an inscription at inception with an inscrption attribute" do
       door.must_respond_to(:inscription)
     end
 
     # it "should not allow for its inscription to be written over" do
-    #
-    # end
+    # proc {door.inscription = "new inscrption"}.must_raise(ArgumentError)
+    # end <== if attr accessor, not reader as currently set up
   end
 
   describe "#locked?" do
-    let(:door) {Door.new("this is a door.")}
+    let(:dave) {Door.new("this is a door")}
     it "should be locked at default" do
-      door.locked?.must_equal(true)
+      # @locked_door == true
+      puts "#{dave.locked_door}"
+      dave.locked?.must_equal(true)
+    end
+
+    it "should tell user a door is unlocked when it's unlocked" do
+      dave.unlock
+      dave.locked?.must_equal(false)
     end
   end
 
   describe "#unlock" do
+    # - You may unlock a Door if and only if it is locked
     let(:door) {Door.new("this is a door.")}
     it "should raise an error if the door is already unlocked" do
-      proc {door.lock = false}.must_raise Exception
+      proc {
+        door.locked_door = false
+        door.unlock}.must_raise(ArgumentError)
     end
-    it "should be unlock when door is locked" do
+
+    it "should be unlocked when door is locked" do
       door.unlock.must_equal("door is now unlocked. Yay.")
     end
 
-    it "should tell user it is already unlocked when unlocked" do
-      door.lock = false
-      door.unlock.must_equal("Door is already unlocked.")
+    # it "should tell user it is already unlocked when unlocked" do
+    #   door.locked_door = false
+    #   door.unlock.must_equal("Door is already unlocked.")
+    # end
+  end
+
+    describe "#lock" do
+      let(:door) {Door.new("this is a door.")} #this is already locked
+      it " 1) should lock an unlocked door" do
+        door.locked_door = false
+        door.lock.must_equal("door is now locked")
+      end
+      # this does not work, why? maybe use .each let
+
+      it "2) should not lock an already locked door" do
+        puts @locked_door
+        proc { door.lock == true}.must_raise(ArgumentError)
+      end
     end
 
 
-  end
 
 end #end class
