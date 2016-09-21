9ed261be3ff07cc3202cd0f5af9bfad920bbcfa4
Alma Maizels <aymaiz@yahoo.com>
2016-09-20 22:13:30 -0700

    Initial file structure set up. Working on project structure.

---
+++ specs/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest'
+require 'minitest/spec'
+require "minitest/autorun"
+require "minitest/reporters"
+require 'minitest/pride'
+
+require_relative '../far_mar'
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
