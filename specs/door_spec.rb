8f9bf0e844937f90e733d0609cbe0dc0ca8303a0
Sarah Nahmias <nahmisa@gmail.com>
2016-09-20 20:53:34 -0700

    Renames test door instances for readability

--- specs/door_spec.rb
+++ specs/door_spec.rb
@@ -3,9 +3,9 @@ require_relative './spec_helper'
 describe Door do
   # Door instance that is opened and locked
   # with default inscription
-  let(:door) { Door.new(true, true) }
+  let(:open_locked_door) { Door.new(true, true) }
   let(:inscribed_door) { Door.new(true, true, "None Shall Write!") }
-  let(:closed_unlocked) {Door.new(false, false)}
+  let(:closed_unlocked_door) {Door.new(false, false)}
 
 
   it "Exists" do
@@ -16,19 +16,19 @@ describe Door do
   describe "Door#is_open" do
     # Closed
     it "Can be either open or closed" do
-      refute_nil(door.is_open)
+      refute_nil(open_locked_door.is_open)
     end
   end
   describe "Door#is_locked" do
     # Check whether or not it is locked,
     it "Can be either locked or unlocked" do
-      refute_nil(door.is_locked)
+      refute_nil(open_locked_door.is_locked)
     end
   end
   describe "Door#inscription" do
     # And look at the writing on the Door if there is any.
     it "Can have an inscription" do
-      refute_nil(door.inscription)
+      refute_nil(open_locked_door.inscription)
     end
   end
 
@@ -38,7 +38,7 @@ describe Door do
     # it cannot be changed
     words = "Hello World!"
     it "Can be inscribed" do
-      assert_equal words, door.inscribe(words)
+      assert_equal words, open_locked_door.inscribe(words)
     end
 
     it "Inscriptions on doors cannot be changed" do
@@ -51,43 +51,43 @@ describe Door do
     # unlocked and
     # closed
     it "Cannot be opened if already open" do
-      assert_raises(Exception) {door.open}
+      assert_raises(Exception) {open_locked_door.open}
     end
 
     it "Must be closed and unlocked to be opened" do
-      assert_equal true, closed_unlocked.open
+      assert_equal true, closed_unlocked_door.open
     end
   end
 
   describe "Door#close" do
     # May close a Door if and only if it is open
     it "Cannot be closed if it already closed" do
-      assert_raises(Exception) {closed_unlocked.close}
+      assert_raises(Exception) {closed_unlocked_door.close}
     end
 
     it "Must be opened to be closed" do
-      assert_equal false, door.close
+      assert_equal false, open_locked_door.close
     end
   end
 
   describe "Door#lock" do
     # May lock a Door if and only if it is unlocked
     it "Cannot be locked if it is already locked" do
-      assert_raises(Exception) {door.lock}
+      assert_raises(Exception) {open_locked_door.lock}
     end
 
     it "Must be unlocked to be locked" do
-      assert_equal true, closed_unlocked.lock
+      assert_equal true, closed_unlocked_door.lock
     end
   end
 
   describe "Door#unlock" do
     # May unlock a Door if and only if it is locked
     it "Cannot be unlocked if it is already unlocked" do
-      assert_raises(Exception) {closed_unlocked.unlock}
+      assert_raises(Exception) {closed_unlocked_door.unlock}
     end
     it "Must be locked to be unlocked" do
-      assert_equal false, door.unlock
+      assert_equal false, open_locked_door.unlock
     end
   end
 
