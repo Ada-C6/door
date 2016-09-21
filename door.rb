871924e9a3242428f83271084bb45598ff03f2de
Karin Kubischta <karin.kubischta@gmail.com>
2016-09-21 11:26:42 -0700

    inscribe tests pass, added open door method

--- door.rb
+++ door.rb
@@ -2,8 +2,48 @@ class Door
   attr_reader :inscription, :position, :security
 
   def initialize
-    @inscription = false
+    @inscription = nil
     @position = "closed"
     @security = "locked"
   end
+
+  def inscribe(message)
+    # I want to rescue the error, but I'm not sure how to write the test if I rescue it.
+    # begin
+      if @inscription == nil && @position == "closed"
+        @inscription = message
+      elsif @position == "open"
+        raise RuntimeError.new("Door cannot be inscribed while it's open.")
+      else
+        raise RuntimeError.new("Door is already inscribed.")
+      end
+    # rescue Exception => m
+    #   return m.message
+    # end
+  end
+
+  def open_door
+    if @position == "closed" && @security == "unlocked"
+      @position = "open"
+    elsif @security == "locked"
+      raise RuntimeError.new("Door is locked.")
+    else
+      raise RuntimeError.new("Door is already open.")
+    end
+  end
+
+  def close_door
+
+  end
+
+  def unlock_door
+    if @security == "locked"
+      @security = "unlocked"
+    end
+  end
+
+  def lock_door
+
+  end
+
 end
--- specs/door_spec.rb
+++ specs/door_spec.rb
@@ -31,25 +31,64 @@ describe Door do
       @my_door.inscription.must_equal("Karin!")
     end
     it "should return an error if door is already inscribed" do
-      skip
       # A door can only be inscribed once
       @my_door.inscribe("First!")
       proc {@my_door.inscribe("Second.")}.must_raise(RuntimeError)
     end
     it "should return an error if door is open" do
-      skip
       # A door object can only be inscribed if it's closed (it would be a pain to try and inscribe an open door, what with it swinging back and forth.)
+      @my_door.unlock_door
       @my_door.open_door
       proc {@my_door.inscribe("Can I inscribe an open door?")}.must_raise(RuntimeError)
     end
   end
 
+  describe "#open_door" do
+    before (:each) do
+      @door_too = Door.new
+    end
+    # - be either open or closed, and
+    # - You may open a Door if and only if it is unlocked and closed
+    it "should return true if successful" do
+      skip
+      @door_too.unlock_door
+      @door_too.open_door.must_equal(true)
+    end
+    it "should return an error if door is locked" do
+      proc {@door_too.open_door}.must_raise(RuntimeError)
+    end
+    it "should return an error if already open" do
+      skip
+      @door_too.unlock_door
+      @door_too.open_door
+      proc {@door_too.open_door}.must_raise(RuntimeError)
+    end
+  end
+
+  describe "#close_door" do
+    # - You may close a Door if and only if it is open
+  end
+
+  describe "#unlock_door" do
+    # You may unlock a Door if and only if it is locked
+    # It does not matter if the door is open or closed to unlock it.
+
+  end
+
+  describe "#lock_door" do
+    # You may lock a Door if and only if it is unlocked
+    # If you lock a Door while it's open, you cannot close it until you unlock it
+
+  end
+
   describe "#inscription?" do
-    it "should return 'no inscription' if inscription is false" do
+    it "should return 'no inscription' if inscription is nil" do
+      skip
       # If door object is not inscribed, what should happen?
     end
 
     it "should return the inscription message" do
+      skip
       # If inscribed, return the message.
     end
 
@@ -58,16 +97,11 @@ end
 
 # A `Door` object can
 #
-# - be either open or closed, and
 # - be either locked or unlocked
 #
 # Here are some rules about how Doors work:
 #
 # - Once the writing (inscription) on a Door is set, it cannot be changed
-# - You may open a Door if and only if it is unlocked and closed
-# - You may close a Door if and only if it is open
-# - You may lock a Door if and only if it is unlocked
-# - You may unlock a Door if and only if it is locked
 # - You should be able to check whether or not a Door is closed, check whether or not it is locked, and look at the writing on the Door if there is any.
 #
 # Appropriate error messages should be displayed and no changes to the Door should be made, if any conditions of the functions are violated.
