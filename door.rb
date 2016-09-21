d460f92c0646507f8e61addfbc0e92f1cd9e47fd
Johna N. Morris <johna.n.morris@gmail.com>
2016-09-20 17:09:28 -0700

    rewrote read method and changed initial value of @inscription to an empty string. Rewrote methods to raise RuntimeErrors. Wrote methods and tests for close_door, unlock_door.

--- door.rb
+++ door.rb
@@ -1,35 +1,66 @@
+# TODO:
+# - Write error messages (asked Dan: should these error messages be raised or can they just be puts??)
+
 class Door
   attr_reader :inscription, :opened, :locked
 
   def initialize
-    @inscription = "BLANK"
+    @inscription = ""
     @opened = false
     @locked = false
-
   end
 
   def inscribe(words)
-    unless @inscription != "BLANK"
+    if @inscription != ""
+      raise RuntimeError.new("You can't inscribe a door that's already been inscribed upon.")
+    else
       @inscription = words
     end
     return @inscription
   end
 
   def read
+    if inscription == ""
+      puts "There's nothing on the door."
+    end
     return @inscription
   end
 
   def open_door
-    if @opened == false && @locked == false
+    if @opened == true
+      raise RuntimeError.new("This door is already open.")
+    elsif @locked == true
+      raise RuntimeError.new("This door is locked.")
+    else
       @opened = true
     end
     return @opened
   end
 
+  def close_door
+    if @opened == false
+      raise RuntimeError.new("This door is already closed.")
+    else
+      @opened = false
+    end
+  end
+
   def lock_door
-    if @locked == false && @opened == false
+    if @locked == true
+      raise RuntimeError.new("This door is already locked.")
+    elsif @opened == true
+      raise RuntimeError.new("You cannot lock an open door.")
+    else
       @locked = true
     end
     return @locked
+  end
+
+  def unlock_door
+    if @locked == false
+      raise RuntimeError.new("This door is already unlocked.")
+    else
+      @locked = false
+    end
   end
 end
--- specs/door_spec.rb
+++ specs/door_spec.rb
@@ -12,6 +12,18 @@ describe Door do
         @door.must_be_instance_of(Door)
     end
 
+    it "should say if a Door is open" do
+      @door.must_respond_to(:opened)
+    end
+
+    it "should say if a Door is locked" do
+      @door.must_respond_to(:locked)
+    end
+
+    it "should say if there is writing on the Door" do
+      @door.must_respond_to(:inscription)
+    end
+
   end
 
   describe "#inscribe(words)" do
@@ -20,37 +32,20 @@ describe Door do
       @door.inscription.must_equal("something")
     end
 
-    # I thought about raising a RuntimeError here, and tested it that way
-    # at first, but thought for a game that that wasn't a particularly
-    # "graceful" or useful error.
-    it "should not allow someone to write on a door which has already been written on" do
+    it "should not prevent writing on a door that's already been written on" do
       @door.inscribe("something")
-      @door.inscribe("something else").must_equal("something")
+      proc { @door.inscribe("something else") }.must_raise(RuntimeError)
     end
   end
 
   describe "#read" do
     # I know the attr_reader will handle this just fine without another
-    # method. However, I initially tested it with @inscription equal to
-    # nil. It turns out, though, that that came with complications of
-    # its own. My first test block, for example, failed if nothing was
-    # written in, because it was returning a string "THIS DOOR IS BLANK"
-    # instead of nil. Setting @inscription to be "BLANK" initially solved
-    # some of this, though I am not entirely happy with this solution either;
-    # as unlikely as it seems, if someone did want to inscribe "BLANK" (I
-    # don't know why they would, but...), they could inscribe something else
-    # afterwards. However, the other problems seemed a bit more likely and
-    # more important to solve.
-    # So, even though an attr_reader is all one would need, I'm keeping
-    # this method, mostly because it makes sense to me from a design
-    # perspective.
+    # method. But I'm keeping this method, mostly because it makes sense
+    # to me from a design perspective, and also allows me to puts a more
+    # useful descriptor for an empty @inscription value.
 
     it "must return the inscription" do
       @door.read.must_equal(@door.inscription)
-    end
-
-    it "should say 'BLANK' if a door hasn't been written on" do
-      @door.read.must_equal("BLANK")
     end
   end
 
@@ -62,7 +57,12 @@ describe Door do
 
     it "should not allow a user to open a locked door" do
       @door.lock_door
-      @door.open_door.must_equal(false)
+      proc {@door.open_door}.must_raise(RuntimeError)
+    end
+
+    it "should not allow a user to open a door that's already open" do
+      @door.open_door
+      proc { @door.open_door }.must_raise(RuntimeError)
     end
   end
 
@@ -74,7 +74,7 @@ describe Door do
 
     # My reasoning:
     # If the lock is a deadbolt and the door is open,
-    # locking a deadbolt does not effectively lock the
+    # locking a deadbolt does not really lock the
     # door. If anything, since it prevents the door from
     # closing, it's worse than unlocked.
     # If the lock is not a deadbolt, locking such a door
@@ -84,8 +84,35 @@ describe Door do
     # an open door in the first place, as many often do.
     it "should not lock an open door" do
       @door.open_door
+      proc { @door.lock_door }.must_raise(RuntimeError)
+    end
+
+    it "should not lock a door that is already locked" do
       @door.lock_door
+      proc { @door.lock_door }.must_raise(RuntimeError)
+    end
+  end
+
+  describe "#close_door" do
+    it "must close an open door" do
+      @door.open_door
+      @door.close_door.must_equal(false)
+    end
+
+    it "must not close a door that is already closed" do
+      proc { @door.close_door }.must_raise(RuntimeError)
+    end
+  end
+
+  describe "#unlock_door" do
+    it "must unlock a locked door" do
+      @door.lock_door
+      @door.unlock_door
       @door.locked.must_equal(false)
+    end
+
+    it "should not unlock a door that is unlocked" do
+      proc {@door.unlock_door}.must_raise(RuntimeError)
     end
   end
 
