1a90d90c4e9cb6033ca223ef7ab05827e8d90a14
Shari Meggs <smeggs1@gmail.com>
2016-09-21 21:03:18 -0700

    I added several methods that involve closing the door including slam and swing.

--- lib/main_door.rb
+++ lib/main_door.rb
@@ -1,7 +1,12 @@
+require_relative 'open_door'
+require_relative 'closed_door'
+
 class MainDoor
 attr_reader :name
+attr_accessor :state
   def initialize
     @name = name
+   @state = "open"
   end
 
   def inscribe (name)
@@ -19,6 +24,34 @@ attr_reader :name
       raise ArgumentError.new("Only enter in letters! This door is sensitive!")
     elsif name !~ /[[:alpha:]]/
       raise ArgumentError.new("Just because it is a string, doesn't mean your entry is valid. Letters only please!")
+    end
+  end
+
+  def check_state
+    return @state
+  end
+
+  def slam
+    closed?
+    @state = "closed"
+
+  end
+
+  def swing
+    closed?
+    counter = rand(1..9) + 1
+    if counter % 3 == 0
+      @state = "open"
+      return "Oops this door is a little tricky. Looks like the door is #{@state}"
+    else
+      @state = "closed"
+      return "The door is now #{@state}"
+    end
+  end
+
+  def closed?
+    if @state == "closed"
+      raise ArgumentError.new("The door is already closed. You can't do that to a closed door")
     end
   end
 end
