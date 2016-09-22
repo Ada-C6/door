aa50328dd447b87530134e83c1a92b240bd5f223
laurenfb <laurenfriesbrink@gmail.com>
2016-09-22 12:40:37 -0700

    clarifying parameters for new Doors

--- lib/door.rb
+++ lib/door.rb
@@ -1,6 +1,9 @@
 class Door
   attr_accessor :inscription, :opened, :locked
 
+
+  # new doors are by default created with no inscription.
+  # they are closed and locked by default, but that can be changed if the creator prefers.
   def initialize(inscription = nil, opened = false, locked = true)
     @inscription = inscription
     @opened = opened
@@ -13,6 +16,6 @@ class Door
 
   def locked?
     return @locked
-  end     
+  end
 
 end
