ccfcda4544ec08a320b0b3df540a4393aac2b005
Alma Maizels <aymaiz@yahoo.com>
2016-09-21 17:53:43 -0700

    Inspect method tested and passed. Now working on Inscribe method.

--- door.rb
+++ door.rb
@@ -3,18 +3,38 @@ module Dungeon
 
     attr_accessor :lock_status, :inscription, :closed
 
+    attr_reader :key_id
+
 
     # initialize with door closed, locked/unlocked, and with/without inscription
-    def initialize(lock_status, inscription = nil)
+    def initialize(lock_status, key_id, inscription = nil)
 
       @closed = true
-      @lock_status = lock_status
       @inscription = inscription
+      @key = key_id #key identifier for lockable doors, key_id == nil means lock_status cannot change
+      @lock_status = lock_status #true for locked
 
       @lock_status == true ? init_state = "locked" : init_state = "unlocked"
-      puts "Here stands an ancient and #{ init_state } sturdy door"
+      puts "Here stands an ancient and sturdy door. It appears to be #{ init_state }."
       if @inscription != nil
         puts "The inscription over the door reads: '#{ @inscription }''"
+      end
+    end
+
+    def inspect
+      if @inscription == nil #no inscription
+        if @closed == true
+          return "Here stands an ancient and sturdy door. It is closed #{ @lock_status == true ? "and" : "but" } appears to be #{ @lock_status == true ? "locked" : "unlocked" }."
+        else # open door
+          return "Here stands an ancient and sturdy door wide open."
+        end
+      else #inscription exists
+        if @closed == true
+          return "Here stands an ancient and sturdy door. It is closed #{ @lock_status == true ? "and" : "but" } appears to be #{ @lock_status == true ? "locked" : "unlocked" }. The door has a sign with the inscription: #{ @inscription }"
+        else # open door
+          return "Here stands an ancient and sturdy door wide open. The door has a sign with the inscription: #{ @inscription }"
+        end
+
       end
 
     end
--- specs/door_spec.rb
+++ specs/door_spec.rb
@@ -8,7 +8,7 @@ module Dungeon
   describe Door do
 
     describe "#initialize" do
-      let(:door) { Door.new(:lock_status) }
+      let(:door) { Door.new(:lock_status, :key_id) }
 
       it "can create an object of Door" do
         door.must_be_instance_of(Door)
@@ -16,23 +16,54 @@ module Dungeon
 
       it "should respond to parameters of door status" do
         door.must_respond_to(:lock_status)
+        door.must_respond_to(:key_id)
         door.must_respond_to(:inscription)
       end
 
-    it "should be a closed door that initializes" do
-      door.closed.must_equal(true)
-    end
+      it "should be a closed door that initializes" do
+        door.closed.must_equal(true)
+      end
 
     end #init
 
-    describe "#the rest" do
+    describe "#inspect" do
+      let(:unlocked_door_no_inscript) { Door.new(false, nil) }
+      let(:locked_door_no_inscript) { Door.new(true, "ABC123") }
+      let(:unlocked_door_with_inscript) { Door.new(false, nil, "Push to open") }
+      let(:locked_door_with_inscript) { Door.new(true, "ABC123", "Use key to open") }
 
+      it "should be possible to inspect a closed door without an inscription for its state of locked/unlocked" do
+        unlocked_door_no_inscript.inspect.must_equal("Here stands an ancient and sturdy door. It is closed but appears to be unlocked.")
+        locked_door_no_inscript.inspect.must_equal("Here stands an ancient and sturdy door. It is closed and appears to be locked.")
+      end
 
+      it "should be possible to inspect a closed door with an inscription for its state of locked/unlocked" do
+        unlocked_door_with_inscript.inspect.must_equal("Here stands an ancient and sturdy door. It is closed but appears to be unlocked. The door has a sign with the inscription: Push to open")
+        locked_door_with_inscript.inspect.must_equal("Here stands an ancient and sturdy door. It is closed and appears to be locked. The door has a sign with the inscription: Use key to open")
+      end
 
+      it "should be possible to inspect an open door without/with an inscription for its state" do
+        skip # @todo until method to open_door available
+        wide_open_door_no_inscript = unlocked_door_no_inscript.open_door
+        wide_open_door_no_inscript.inspect.must_equal("Here stands an ancient and sturdy door wide open.")
+        wide_open_door_with_inscript = unlocked_door_with_inscript.open_door
+        wide_open_door_with_inscript.inspect.musmust_equal("Here stands an ancient and sturdy door wide open. The door has a sign with the inscription: Push to open")
+      end
+
+    end #inspect
+
+    describe "#inscription" do
       it "should display an inscription, if there is one written" do
         skip
       end
 
+      it "should be possible to inscribe the door only once" do
+        skip
+      end
+
+    end #inscription
+
+    describe "open" do
       it "should be possible to open, when closed and unlocked" do
         skip
       end
@@ -41,27 +72,27 @@ module Dungeon
         skip
       end
 
-      it "should be possible to lock or unlock, if closed, by switching state" do
+    end #open
+
+    describe "#turn_key" do
+
+      it "if closed, should be possible to lock or unlocked, by switching state" do
         skip
       end
 
-      it "should be possible to inscribe the door only once" do
-        skip
-      end
+    end
 
-      it "should be possible to inspect a door for its state of open/closed" do
-        skip
-      end
+    describe "#closed?" do
 
-      it "should be possible to inspect a door for its state of locked/unlocked" do
-        skip
-      end
+    end
 
-      it "should be possible to inspect a door to read the inscription" do
-        skip
-      end
+    describe "locked?" do
+
+
+
 
       it "should give a warning message if the door is used in the wrong way" do
+        # missing key?
         # check for warning for opening a locked door without first unlocked
         # check for warning for changing the inscription when there is already once
         # check for warning for closing/opening an already closed/open door
