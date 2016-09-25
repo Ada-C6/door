af3faa4bfda1eb0904b4f6a22a30394f6b385f39
Brandi Phillips <milligan.brandi@gmail.com>
2016-09-25 14:56:55 -0700

    door seems to be working, can't get the specs onboard.

--- lib/door.rb
+++ lib/door.rb
@@ -4,7 +4,7 @@ class Door
 
   attr_reader :inscription
   attr :state, :l_state
-# upon initializing any door, the default door state is closed and locked.
+# upon initializing any door, the default door state is closed and locked.  User must set inscription upon initializing.
   def initialize (inscription, state="closed", l_state="locked")
     @inscription = inscription
     @state = state
@@ -12,44 +12,37 @@ class Door
   end
 
   def open_door
-    raise StatusError, "Door already open" if @state == "open"
-    raise LStatusError, "The door is locked" if @l_state == "locked"
-    if @state == "closed" && @l_state == "unlocked"
-      @state= "open"
-      puts "You have opened the door"
-    end
-    return @state
+    raise DoorErrors::LStatusError, "The door is locked" if @l_state == "locked"
+    raise DoorErrors::StatusError, "Door already open" if @state == "open"
+    puts "You have opened the door"
+    @state= "open"
   end
 
   def close_door
-    raise StatusError, "Door already closed" if @state == "closed"
-    if @state == "open"
+    raise DoorErrors::StatusError, "Door already closed" if @state == "closed"
+    puts "You have closed the door"
       @state= "closed"
-      puts "You have closed the door"
-    end
-    return @state
   end
 
   def lock_door
-    raise LStatusError, "The door is locked" if @l_state == "locked"
-    if @l_state == "unlocked"
-      @l_state = "locked"
-      puts "you have locked the door"
-    end
-    return @state
+    raise DoorErrors::LStatusError, "The door is locked" if @l_state == "locked"
+    puts "you have locked the door"
+    @l_state = "locked"
   end
 
   def unlock_door
-    raise LStatusError, "Door already unlocked" if @l_state == "unlocked"
-    if @l_state == "locked"
-      @l_state = "unlock"
-      puts "You have unlocked the door"
-    end
-    return @state
+    raise DoorErrors::LStatusError, "Door already unlocked" if @l_state == "unlocked"
+    puts "You have unlocked the door"
+    @l_state = "unlocked"
   end
-
 
 end
 
 # portal = Door.new("A New World")
-# puts portal.inscription
+# # puts portal.inscription
+#
+# portal.unlock_door
+#
+# portal.open_door
+#
+# puts portal.state
