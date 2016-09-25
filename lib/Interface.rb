04578700aaffff29218eca95347da5a09b5ae0d8
Maya Wang <salome.neptune@gmail.com>
2016-09-25 16:56:39 -0700

    moved the play logic to main class, linking to door and UI

--- lib/Interface.rb
+++ lib/Interface.rb
@@ -113,21 +113,4 @@ class Interface
       show_status(door)
     end
   end
-
-
-
 end
-
-ui = Interface.new
-begin
-  door = Door.new
-rescue Door::IsOpenAndLockedError
-  puts "The door can not be initialized with both open and locked."
-rescue Door::InscriptionIsNotString
-  puts "The inscription can not be initialized with a non-string except nil."
-rescue Door::IsOpenIsNotBoolean
-  puts "The door's open or close status can not be initialized with a non-boolean."
-rescue Door::IsLockedIsNotBoolean
-  puts "The door's lock or unlocked status can not be initialized with a non-boolean."
-end
-ui.play(door)
