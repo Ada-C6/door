f64b8755aa925ed5dc38fc0639334ab6f0b5c34d
Yasmin <yasminor@gmail.com>
2016-09-25 16:24:06 -0700

    Fixed project directory structure issues

---
+++ door.rb
@@ -0,0 +1,23 @@
+class Door
+  # attr_reader :open_status :locked_status
+  #
+  # def initialize
+  #
+  # end
+  #
+  # def open
+  #
+  # end
+  #
+  # def close
+  #
+  # end
+  #
+  # def lock
+  #
+  # end
+  #
+  # def unlock
+  # 
+  # end
+end
--- specs/door_spec.rb
+++ specs/door_spec.rb
@@ -1,1 +1,31 @@
 require_relative 'spec_helper'
+require_relative '../door.rb'
+
+describe Door do
+  describe "#initialize" do
+
+    it "should create a new instance of Door" do
+      a_door = Door.new(False, False)
+      a_door.must_be_instance_of(Door)
+    end
+
+    it "should intialize with an open_status property" do
+
+    end
+
+    it "should have an open_status property of either true or false" do
+
+    end
+
+    it "should initialize with a locked_status property" do
+
+    end
+
+    it "should have a locked_status property of either true or false" do
+
+    end
+
+  end
+
+
+end
