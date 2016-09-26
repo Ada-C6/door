71e43c2e3eafc82736901b642ff3ce4d4d7cc7de
Yasmin <yasminor@gmail.com>
2016-09-25 21:50:09 -0700

    Added tests for 100% coverage

--- specs/door_spec.rb
+++ specs/door_spec.rb
@@ -15,17 +15,14 @@ describe Door do
       @a_door.must_respond_to(:open_status)
     end
 
-    # it "should have an open_status property of either true or false" do
-    #   :open_status.must_be_kind_of(Boolean)
-    # end
-
     it "should have a locked_status property" do
       @a_door.must_respond_to(:locked_status)
     end
-    #
-    # # it "should have a locked_status property of either true or false" do
-    # #
-    # # end
+
+    it "should tell a user if the door is open or closed" do
+      @a_door.open_status.must_equal(false)
+    end
+
   end
 
   describe "#open" do
@@ -41,48 +38,72 @@ describe Door do
       proc { door_two.open }.must_raise(ArgumentError)
     end
 
-    # it "should open the door by changing the open_status to true" do
-    #   door_one.open
-    #   door_one.open_status.must_equal(true)
-    # end
+    it "should change the open_status to open if the door was unlocked and closed" do
+      door_one.open.must_equal(true)
+    end
   end
 
   describe "#close" do
     let(:closed_door) {Door.new(false, false)} #closed and unlocked
+    let(:door_z) {Door.new(true, false)}
 
     it "should raise an error if the door is already closed" do
       closed_door = Door.new(false, false)
       proc { closed_door.close }.must_raise(ArgumentError)
     end
 
-    # it "should close the door by changing the open_status to false" do
-    #
-    # end
+    it "should change the open_status to closed if the door was open" do
+      door_z.close.must_equal(false)
+    end
   end
 
   describe "#lock" do
     let(:locked_door) {Door.new(false, true)} #closed and locked
+    let(:open_door) {Door.new(true, false)}
+    let(:unlocked_door) {Door.new(false, false)}
 
     it "should raise an error if the door is already locked" do
       proc { locked_door.lock }.must_raise(ArgumentError)
     end
 
-    # it "should lock the door by changing the locked_status to true" do
-    #   a_door = Door.new(false, false)
-    #   a_door.lock.must_equal(true)
-    # end
+    it "should raise an error if the door is already open" do
+      proc { open_door.open }.must_raise(ArgumentError)
+    end
+
+    it "should change locked_status if previously unlocked" do
+      unlocked_door.lock.must_equal(true)
+    end
   end
 
   describe "#unlock" do
     let(:closed_unlocked_door) {Door.new(false, false)} #closed and unlocked
+    let(:open_door) {Door.new(true, false)}
+    let(:locked_door) {Door.new(false, true)}
 
     it "should raise an error if the door is already unlocked" do
       proc { closed_unlocked_door.unlock }.must_raise(ArgumentError)
     end
 
-    # it "should unlock the door by changing the locked status to false" do
-    #
-    # end
+    it "should raise an error if the door is already open" do
+      proc { open_door.unlock }.must_raise(ArgumentError)
+    end
+
+    it "should change locked_status if previously locked" do
+      locked_door.unlock.must_equal(false)
+    end
+  end
+
+  describe "#inscribe" do
+    let(:door_x) {Door.new(false, true, "La Puerta")}
+    let(:door_y) {Door.new(false, true)}
+
+    it "should raise an error if an inscription change is attempted" do
+      proc { door_x.inscribe("Home") }.must_raise(ArgumentError)
+    end
+
+    it "should change the inscription if one hasn't been set yet" do
+      door_y.inscribe("Home")
+    end
   end
 
   describe "#read" do
