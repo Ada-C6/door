cd35a4e26523e3cf10c0e85f769febe3fcb8348b
Yasmin <yasminor@gmail.com>
2016-09-25 17:12:15 -0700

    added open and close methods as well as tests for these methods

--- door.rb
+++ door.rb
@@ -1,23 +1,28 @@
 class Door
-  # attr_reader :open_status :locked_status
+  attr_reader :open_status, :locked_status
   #
-  # def initialize
-  #
-  # end
-  #
-  # def open
-  #
-  # end
-  #
-  # def close
-  #
-  # end
-  #
+  def initialize(open_status, locked_status)
+
+  end
+
+  def open
+    raise ArgumentError.new("The door is already open!") unless @open_status == false
+    raise ArgumentError.new("The door is locked!") unless @locked_status == false
+    @open_status = true
+    return @open_status
+  end
+
+  def close
+    raise ArgumentError.new("The door is already closed!") unless @open_status == true
+    @open_status = false
+    return @open_status
+  end
+
   # def lock
   #
   # end
   #
   # def unlock
-  # 
+  #
   # end
 end
--- specs/door_spec.rb
+++ specs/door_spec.rb
@@ -3,29 +3,58 @@ require_relative '../door.rb'
 
 describe Door do
   describe "#initialize" do
+    # before (:each) do
+    #   a_door = Door.new#(False, False)
+    # end
 
     it "should create a new instance of Door" do
-      a_door = Door.new(False, False)
+      a_door = Door.new(false, false)
       a_door.must_be_instance_of(Door)
     end
 
-    it "should intialize with an open_status property" do
-
+    it "should have an open_status property" do
+      a_door = Door.new(false, true)
+      a_door.must_respond_to(:open_status)
     end
 
-    it "should have an open_status property of either true or false" do
+    # it "should have an open_status property of either true or false" do
+    #   :open_status.must_be_kind_of(Boolean)
+    # end
 
+    it "should have a locked_status property" do
+      a_door = Door.new(true, false)
+      a_door.must_respond_to(:locked_status)
     end
-
-    it "should initialize with a locked_status property" do
-
-    end
-
-    it "should have a locked_status property of either true or false" do
-
-    end
-
+    #
+    # # it "should have a locked_status property of either true or false" do
+    # #
+    # # end
   end
 
+  describe "#open" do
+    it "should raise an error if the door is already open" do
+      a_door = Door.new(true, false)
+      proc { a_door.open }.must_raise(ArgumentError)
+    end
 
+    it "should raise an error if the door is locked" do
+      a_door = Door.new(false, true)
+      proc { a_door.open }.must_raise(ArgumentError)
+    end
+
+    # it "should open the door by changing the open_status to true" do
+    #
+    # end
+  end
+
+  describe "#close" do
+    it "should raise an error if the door is already closed" do
+      a_door = Door.new(false, false)
+      proc { a_door.open }.must_raise(ArgumentError)
+    end
+
+    # it "should close the door by changing the open_status to false" do
+    #
+    # end
+  end
 end
