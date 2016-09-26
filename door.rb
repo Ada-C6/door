f7136e2076f18ecb2104b8921dd8e97f6750ad4e
Mary Morrison <marymorrison15@gmail.com>
2016-09-25 20:09:09 -0700

    couldn't follow flow, very messy, issues with ArgumentError formatting/tests, STARTING OVER. So far, just have initialize test and method.

--- door.rb
+++ door.rb
@@ -1,57 +1,10 @@
 class Door
-  attr_accessor :locked, :open_door, :inscription
+  attr_reader :closed, :locked, :inscription
 
-  # initialized door, closed and locked with no inscription
-  def initialize(lock, open)
-    @locked = true
-    @open = false
+  def initialize
+    @closed = true
+    @locked = false
     @inscription = nil
   end
 
-  def inscribe(message)
-    # Once the writing (inscription) on a Door is set, it cannot be changed
-    # Appropriate error messages should be displayed and no changes to the Door should be made, if any conditions of the functions are violated.
-    if @inscription == nil
-      @inscription = message
-    else
-      raise ArgumentError.new("A door already has an inscription.")
-    end
-  end
-
-  def open_door
-    # You may open a Door if and only if it is unlocked and closed
-    # Appropriate error messages should be displayed and no changes to the Door should be made, if any conditions of the functions are violated.
-    if @locked == false && @open == true
-      raise ArgumentError.new("A door is already open.")
-    elsif @locked == true && @open == false
-      raise ArgumentError.new("A door must be unlocked before it can be opened.")
-    elsif @locked == true && @open == true
-      raise ArgumentError.new("A door must be closed and unlocked before it can be open.")
-    else @open = true
-      return "A door has been opened."
-    end
-  end
-
-  def close_door
-    # You may close a Door if and only if it is open
-    # Appropriate error messages should be displayed and no changes to the Door should be made, if any conditions of the functions are violated.
-    if @locked == false && @open == false
-      raise ArgumentError.new("A door is already closed.")
-    elsif @locked == true && @open == true
-      raise ArgumentError.new("A door must be unlocked before it can be closed.")
-    elsif @locked == true && @open == false
-      raise ArgumentError.new("A door must be opened and unlocked before it can be open.")
-    else @open = false
-      return "A door has been closed."
-    end
-  end
-
-  def lock_door
-    if @locked == true
-      raise ArgumentError.new("A door is already locked.")
-    else @locked = true
-      return "A door has been locked."
-    end
-  end
-
-end
+end 
--- specs/door_spec.rb
+++ specs/door_spec.rb
@@ -1,108 +1,13 @@
 require_relative 'spec_helper'
-require_relative '../door'
+require_relative '../door.rb'
 
 describe Door do
-  before (:all) do
-    @d = Door.new(true, false)
-  end
+  let(:door) { Door.new }
 
   describe "#initialize" do
-
-    it "should create a new instance of Door" do
-      @d.must_be_instance_of(Door)
+    it "creates a new instance of Door" do
+      door.must_be_instance_of(Door)
     end
-
   end
 
-  describe "#inscribe(message)" do
-    before(:all) do
-      @d.inscribe("HEY GURL")
-    end
-
-    it "should inscribe the correct string" do
-      @d.inscription.must_equal("HEY GURL")
-    end
-
-    it "should raise an ArgumentError if the message is already inscribed" do
-      proc { @d.inscribe("NEW MESSAGE") }.must_raise(ArgumentError)
-    end
-
-  end
-
-  describe "#open_door" do
-
-    it "should raise an ArgumentError if the door was already open" do
-      d = Door.new(false, true)
-      proc { d.open_door }.must_raise(ArgumentError)
-    end
-
-    it "should raise an ArgumentError if the door was locked" do
-      d = Door.new(true, false)
-      proc { d.open_door }.must_raise(ArgumentError)
-    end
-
-    it "should raise an ArgumentError if the door is open and locked" do
-      d = Door.new(true, true)
-      proc { d.open_door }.must_raise(ArgumentError)
-    end
-
-    it "should open the door" do
-      if @open == false && @locked == false
-        d.open_door.must_equal(true)
-      end
-    end
-
-  end
-
-  describe "#close_door" do
-
-    it "should raise an ArgumentError if the door was already closed" do
-      d = Door.new(false, false)
-      proc { d.close_door }.must_raise(ArgumentError)
-    end
-
-    it "should raise an ArgumentError if the door was locked" do
-      d = Door.new(true, true)
-      proc { d.close_door }.must_raise(ArgumentError)
-    end
-
-    it "should raise an ArgumentError if the door is closed and locked" do
-      d = Door.new(true, false)
-      proc { d.close_door }.must_raise(ArgumentError)
-    end
-
-    it "should close the door" do
-      if @open == true && @locked == false
-        d.close_door.must_equal(true)
-      end
-    end
-
-  end
-
-
-      # def initialize(lock, open)
-      #   @locked = true
-      #   @open = false
-      #   @inscription = nil
-      # end
-
-  describe "#lock_door" do
-
-    it "should raise an ArgumentError if the door was already locked" do
-      d = Door.new(true, false)
-      proc { d.lock_door }.must_raise(ArgumentError)
-    end
-
-    it "should lock the door" do
-      d = Door.new(false, true)
-      if @locked == false
-        d.lock_door.must_equal(true)
-      end
-    end
-
-  end
-
-
-
-
-end
+end 
