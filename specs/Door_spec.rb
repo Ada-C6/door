4eb02c0c1459334cf90b2695e6c5eb1cf483ed0b
Jessica <jchung722@gmail.com>
2016-09-26 04:32:45 -0700

    make base files

---
+++ specs/Door_spec.rb
@@ -0,0 +1,19 @@
+#FarMar_spec.rb
+
+require_relative 'Spec_helper'
+require_relative '../lib/Door/'
+
+
+describe 'Testing FarMar' do
+  let(:new_market) { FarMar::Market.new("1","People's Co-op Farmers Market","30th and Burnside","Portland","Multnomah","Oregon","97202") }
+  let(:new_product) { FarMar::Product.new("1","Dry Beets","1") }
+  let(:new_sale) { FarMar::Sale.new("1","9290","2013-11-07 04:34:56 -0800","1","1") }
+  let(:new_vendor) { FarMar::Vendor.new("1","Feil-Farrell","8","1") }
+
+
+#############--------------FarMar::Market Tests--------------------###########
+
+  it "Must return Market class" do
+    expect(new_market).must_be_instance_of(FarMar::Market)
+    expect(new_market.name).must_equal("People's Co-op Farmers Market")
+  end
