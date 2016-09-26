3c08758eeb4f738dca5cad5b258eac87c1082111
Yasmin <yasminor@gmail.com>
2016-09-25 21:50:55 -0700

    Fixed bug in initialize method

--- door.rb
+++ door.rb
@@ -1,10 +1,10 @@
 class Door
   attr_reader :open_status, :locked_status, :inscription
 
-  def initialize(open_status, locked_status, inscription = "")
-    open_status = @open_status
-    locked_status = @locked_status
-    inscription = @inscription
+  def initialize(open_status, locked_status, inscription = nil)
+    @open_status = open_status
+    @locked_status = locked_status
+    @inscription = inscription
   end
 
   def open
@@ -22,25 +22,34 @@ class Door
 
   def lock
     raise ArgumentError.new("The door is already locked!") unless @locked_status == false
+    raise ArgumentError.new("You cannot lock an open door!") unless @open_status == false
     @locked_status = true
     return @locked_status
   end
 
   def unlock
     raise ArgumentError.new("The door is already unlocked!") unless @locked_status == true
+    raise ArgumentError.new("There's no need to unlock an open door!") unless @open_status == false
     @locked_status = false
     return @locked_status
   end
 
-  def read
-    if @inscription != ""
-      puts @inscription
-      return true
+  def inscribe(message)
+    if @inscription == nil
+      @inscription = message
     else
-      puts "There is no inscription on this door."
-      return false
+      raise ArgumentError.new("You may only inscribe once on a door.")
     end
   end
 
+  def read
+    if @inscription == nil
+      puts "There is no inscription on this door."
+      return false
+    else
+      puts @inscription
+      return true
+    end
+  end
 
 end
